57aff95ba893468a7b74c32e22879f3c2bc0cbd5
Aaron Park <ync4hn@virginia.edu>
2024-05-23 18:53:30 -0400

    IT WORKS!!

--- src/main.jsx
+++ src/main.jsx
@@ -6,45 +6,48 @@ const scene = new THREE.Scene(); // creates the scene (NEEDED think like just sc
 
 const renderer = new THREE.WebGLRenderer(); // how it renders onto the  screen
 renderer.setSize(window.innerWidth, window.innerHeight); // sets the size of the render/
-//renderer.setClearColor(0xffffff, 1); //turns the background white
+renderer.setClearColor(0xffffff, 1); //turns the background white
 document.body.appendChild(renderer.domElement); // how the thing actually renders it i think ?
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // Materials
-const material = new THREE.MeshBasicMaterial({ color: 0x00fff00, wireframe: false }); // green sides
+//const material = new THREE.MeshBasicMaterial({ color: 0x00fff00, wireframe: false }); // green sides
 
 // Geometry
 // 3rd one is height
-const geometry = new THREE.CylinderGeometry(0.03, 0.03, 1, 64, 1, false);
+const geometry = new THREE.CylinderGeometry(0.03, 0.03, 2, 64, 1, false);
 
-// FAKE THING FOR TESTING RN
-const material2 =  new THREE.MeshBasicMaterial({ color: 0xffffffff, wireframe: false }); // green sides
-const fake = new THREE.Mesh(geometry, material2);
-scene.add(fake);
-fake.rotation.x = 3.14/2
-fake.position.x = 0.2;
-fake.position.y = 0;
-fake.position.z = 25;
+const rainbowColors = [
+  0xFF0000, // Red
+  0xFF7F00, // Orange
+  0xFFFF00, // Yellow
+  0x00FF00, // Green
+  0x0000FF, // Blue
+  0x4B0082, // Indigo
+  0x9400D3  // Violet
+];
 
 
 // add more and change the heights/positionZ mainly
 const initial = [
-  { rotationX: 3.14 / 2, positionX: 0.42, positionY: 0.25, positionZ: 10 },
-  { rotationX: -3.14 / 2, positionX: -0.3, positionY: 0.34561, positionZ: 15 },
-  { rotationX: -3.14 / 2, positionX: -0.5, positionY: -0.4, positionZ: 6 },
-  { rotationX: 3.14 / 2, positionX: 0.4, positionY: -0.2, positionZ: 13 },
-  { rotationX: 3.14 / 2, positionX: 0, positionY: .38, positionZ: 14 },
-  { rotationX: -3.14 / 2, positionX: 0, positionY: -.35, positionZ: 4 },
+  { rotationX: 3.14 / 2, positionX: 0.22, positionY: 0.21, positionZ: 10 },
+  { rotationX: -3.14 / 2, positionX: -0.2, positionY: 0.204, positionZ: 15 },
+  { rotationX: -3.14 / 2, positionX: -0.23, positionY: -0.2, positionZ: 6 },
+  { rotationX: 3.14 / 2, positionX: 0.21, positionY: -0.2, positionZ: 13 },
+  { rotationX: 3.14 / 2, positionX: 0, positionY: .221, positionZ: 14 },
+  { rotationX: -3.14 / 2, positionX: 0, positionY: -.2045, positionZ: 4 },
   { rotationX: -3.14 / 2, positionX: -0.2, positionY: -.22, positionZ: 7 },
-  { rotationX: -3.14 / 2, positionX: 0.47, positionY: -.11, positionZ: 9 },
-  { rotationX: -3.14 / 2, positionX: -0.36, positionY: -.18, positionZ: 11 },
-  { rotationX: -3.14 / 2, positionX: 0.41, positionY: -.18, positionZ: 2 },
-  { rotationX: -3.14 / 2, positionX: -0.324, positionY: -.28, positionZ: 3 }
+  { rotationX: -3.14 / 2, positionX: 0.21, positionY: -.11, positionZ: 9 },
+  { rotationX: -3.14 / 2, positionX: -0.24, positionY: -.18, positionZ: 11 },
+  { rotationX: -3.14 / 2, positionX: 0.23, positionY: -.18, positionZ: 2 },
+  { rotationX: -3.14 / 2, positionX: -0.211, positionY: -.23, positionZ: 3 }
 ];
 
 // create the initial cylinders towards the beginning of the session
 initial.forEach(initials => {
+  const color = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+  const material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
   const cylinder = new THREE.Mesh(geometry, material);
   scene.add(cylinder);
   cylinder.rotation.x = initials.rotationX;
@@ -52,8 +55,7 @@ initial.forEach(initials => {
   cylinder.position.y = initials.positionY;
   cylinder.position.z = initials.positionZ;
 });
-camera.position.z = 30;
-
+camera.position.z = 23;
 
 
 const positions = [];
@@ -79,7 +81,6 @@ function isMiddle(newX, newY, threshold) {
 }
 
 
-
 // change some of the positionZs and the heights of them to make it better
 const adjustments = [
   { rotationX: 3.14 / 2, positionX: 0.3, positionY: 0.2 },
@@ -90,24 +91,38 @@ const adjustments = [
   { rotationX: -3.14 / 2, positionX: 0, positionY: -.2 }
 ];
 
+
+// adjust this so that its more like LESS EARLY, MORE LATER.
+// i thinkr n its ok but tweak it later?
 adjustments.forEach(adjustment => {
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < 50; i++) {
     // fine tune the numbers later because lowkey want it to be closer to the middle a little bit
     let newX = 0;
     let newY = 0;
-    let newZ = -5 + (Math.random() * 13);
+    let check = Math.random();
+    let newZ = 0.0;
+
+    // randomly decides if the position of the new values will be positive or negative
+    if (check <= 0.4){
+      newZ = -5 + (Math.random() * 5);
+
+    }
+    else{
+      newZ = -5 - (Math.random() * 5);
+
+    }
     
     if (adjustment.positionX < 0){
-      newX = adjustment.positionX + (Math.random() * (i / 22));
+      newX = adjustment.positionX + (Math.random() * (i / 65));
     }
     else{
-      newX = adjustment.positionX - (Math.random() * (i / 22));
+      newX = adjustment.positionX - (Math.random() * (i / 65));
     }  
     if (adjustment.positionY < 0){
-      newY = adjustment.positionY + (Math.random() * (i / 22));
+      newY = adjustment.positionY + (Math.random() * (i / 65));
     }
     else{
-      newY = adjustment.positionY - (Math.random() * (i / 22));
+      newY = adjustment.positionY - (Math.random() * (i / 65));
     }  
 
     // console.log(`Generated newX: ${newX}, newY: ${newY}`); // Log newX and newY
@@ -127,6 +142,8 @@ adjustments.forEach(adjustment => {
 
     // if arent close to each other by diameter length (0.062 with margin of error) -> adds the jauntson
     if (!isPositionClose(newX, newY, 0.062)) {
+      const color = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+      const material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
       const temp = new THREE.Mesh(geometry, material);
       scene.add(temp);
       temp.rotation.x = adjustment.rotationX;
@@ -150,16 +167,16 @@ adjustments.forEach(adjustment => {
       const maxAttempts = 10;
       while (isPositionClose(newX, newY, 0.062) && attempts < maxAttempts) {
         if (adjustment.positionX < 0){
-          newX = adjustment.positionX + (Math.random() * (i / 22));
+          newX = adjustment.positionX + (Math.random() * (i / 65));
         }
         else{
-          newX = adjustment.positionX - (Math.random() * (i / 22));
+          newX = adjustment.positionX - (Math.random() * (i / 65));
         }  
         if (adjustment.positionY < 0){
-          newY = adjustment.positionY + (Math.random() * (i / 22));
+          newY = adjustment.positionY + (Math.random() * (i / 65));
         }
         else{
-          newY = adjustment.positionY - (Math.random() * (i / 22));
+          newY = adjustment.positionY - (Math.random() * (i / 65));
         }  
         attempts++;
         if (attempts >= maxAttempts) {
@@ -173,6 +190,8 @@ adjustments.forEach(adjustment => {
         console.log(`Generated MIDDLE : ${newX}, tempY  : ${newY}`); // Log tempX and tempY
         continue;
       }
+      const color = rainbowColors[Math.floor(Math.random() * rainbowColors.length)];
+      const material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
       const temp = new THREE.Mesh(geometry, material);
       scene.add(temp);
       temp.rotation.x = adjustment.rotationX;
@@ -189,7 +208,7 @@ adjustments.forEach(adjustment => {
 function animate() {
   requestAnimationFrame(animate);
 
-  camera.position.z -= 0.05;
+  camera.position.z -= 0.1;
 
   renderer.render(scene, camera);
 }
